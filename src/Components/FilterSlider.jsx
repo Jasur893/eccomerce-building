433cbd429b9ee8ed8848b36848935789fa9eaa0c
Jasur893 <jasur1993ds@gmail.com>
2023-02-22 12:10:54 +0500

    configure rename

--- src/Components/FilterSlider.jsx
+++ src/Components/FilterSlider.jsx
@@ -3,16 +3,23 @@ import Tabs from 'react-bootstrap/Tabs'
 import FilterNew from './FilterNew'
 import FilterStock from './FilterStock'
 import FilterHitSale from './FilterHitSale'
-
+ 
 export default function FilterSlider() {
+  
+
   return (
     <div className='bg-black'>
       <Tabs
+        variant='pills'
         defaultActiveKey='home'
         id='uncontrolled-tab-example'
-        className='borderTab container_content gap-x-1 md:gap-x-10'
+        className='borderTab container_content py-1 gap-x-1 md:gap-x-10'
       >
-        <Tab className='borderTab bg-dark border-t-0' eventKey='home' title='Новинки'>
+        <Tab
+          className='borderTab bg-dark border-t-0'
+          eventKey='home'
+          title='Новинки'
+        >
           <div className='container_content'>
             <FilterNew />
           </div>
@@ -22,7 +29,11 @@ export default function FilterSlider() {
             <FilterStock />
           </div>
         </Tab>
-        <Tab className='borderTab bg-dark' eventKey='contact' title='Хиты продаж'>
+        <Tab
+          className='borderTab bg-dark'
+          eventKey='contact'
+          title='Хиты продаж'
+        >
           <div className='container_content'>
             <FilterHitSale />
           </div>
