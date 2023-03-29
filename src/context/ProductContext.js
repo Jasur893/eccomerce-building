4eb88f1ec3a63b2ad055a2de2b72c7221d284f09
Jasur893 <jasur1993ds@gmail.com>
2023-03-29 11:46:16 +0500

    fix

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -3,113 +3,32 @@ import { useDispatch, useSelector } from 'react-redux'
 import { collection, getDocs } from 'firebase/firestore'
 import { db } from '../FirebaseConfigs/firebaseConfig'
 import { setProductsALL, setTotal } from '../redux/actions'
+import { getProducts } from '../productApi'
 
 export const ProductContext = React.createContext()
 
 const ProductProvider = ({children}) => {
-  // const [value, dispatch] = useReducer(reducer, initialState)
   const cart = useSelector((state) => state.cart)
   const total = useSelector((state) => state.total)
-
+  const productAll = getProducts()
   const dispatch = useDispatch()
 
   useEffect(() => {
     const getAllProduct = async() => {
       const productsArray = []
-      await getDocs(collection(db, 'products-ГРУНТОВКА')).then((querySnapshot) => {
+      for (let index = 0; index < productAll.length; index++) {
+        await getDocs(collection(db, `${productAll[index]}`)).then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
             productsArray.push({...doc.data(), id: doc.id, isLiked: false, isAdded: false})
           })
         }).catch((error)=> {
           console.log(error.message);
         })
-      await getDocs(collection(db, 'products-ДРЕЛИ')).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          productsArray.push({...doc.data(), id: doc.id, isLiked: false, isAdded: false})
-        })
-      }).catch((error)=> {
-        console.log(error.message);
-      })
-      await getDocs(collection(db, 'products-КРАСТЕЛИ')).then((querySnapshot, ) => {
-        querySnapshot.forEach((doc) => {
-          productsArray.push({...doc.data(), id: doc.id, isLiked: false, isAdded: false})
-        })
-      }).catch((error)=> {
-        console.log(error.message);
-      })
-      await getDocs(collection(db, 'products-МОНТАЖНЫЙ')).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          productsArray.push({...doc.data(), id: doc.id, isLiked: false, isAdded: false})
-        })
-      }).catch((error)=> {
-        console.log(error.message);
-      })
-      await getDocs(collection(db, 'products-НАСОСЫ')).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          productsArray.push({...doc.data(), id: doc.id, isLiked: false, isAdded: false})
-        })
-      }).catch((error)=> {
-        console.log(error.message);
-      })
-      await getDocs(collection(db, 'products-ПЕРФОРАТОРЫ')).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          productsArray.push({...doc.data(), id: doc.id, isLiked: false, isAdded: false})
-        })
-      }).catch((error)=> {
-        console.log(error.message);
-      })
-      await getDocs(collection(db, 'products-ПЕРЧАТКИ')).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          productsArray.push({...doc.data(), id: doc.id, isLiked: false, isAdded: false})
-        })
-      }).catch((error)=> {
-        console.log(error.message);
-      })
-      await getDocs(collection(db, 'products-ПОЛИВОЧНЫЙ ИНВЕНТАРЬ')).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          productsArray.push({...doc.data(), id: doc.id, isLiked: false, isAdded: false})
-        })
-      }).catch((error)=> {
-        console.log(error.message);
-      })
-      await getDocs(collection(db, 'products-СЛЕСАРНЫЙ')).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          productsArray.push({...doc.data(), id: doc.id, isLiked: false, isAdded: false})
-        })
-      }).catch((error)=> {
-        console.log(error.message);
-      })
-      await getDocs(collection(db, 'products-СПЕЦ ОДЕЖДА')).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          productsArray.push({...doc.data(), id: doc.id, isLiked: false, isAdded: false})
-        })
-      }).catch((error)=> {
-        console.log(error.message);
-      })
-      await getDocs(collection(db, 'products-СУХИЕ СМЕСИ')).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          productsArray.push({...doc.data(), id: doc.id, isLiked: false, isAdded: false})
-        })
-      }).catch((error)=> {
-        console.log(error.message);
-      })
-      await getDocs(collection(db, 'products-ШЛИФОВАЛЬНЫЕ МАШИНКИ')).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          productsArray.push({...doc.data(), id: doc.id, isLiked: false, isAdded: false})
-        })
-      })
-      await getDocs(collection(db, 'products-ОБУВЬ')).then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          productsArray.push({...doc.data(), id: doc.id, isLiked: false, isAdded: false})
-        })
-      })
-      .catch((error)=> {
-        console.log(error.message);
-      })
+      }
       dispatch(setProductsALL(productsArray))
     }
     getAllProduct()
-  },[dispatch])
+  },[dispatch, productAll])
 
   useEffect(()=> {
     const total = cart.reduce((accumulator, currentItem)=> {
---
+++ src/productApi.js
@@ -0,0 +1,19 @@
+const products = [
+  'products-ГРУНТОВКА',
+  'products-ДРЕЛИ',
+  'products-КРАСТЕЛИ',
+  'products-МОНТАЖНЫЙ',
+  'products-НАСОСЫ',
+  'products-ПЕРФОРАТОРЫ',
+  'products-ПЕРЧАТКИ',
+  'products-ПОЛИВОЧНЫЙ ИНВЕНТАРЬ',
+  'products-СЛЕСАРНЫЙ',
+  'products-СПЕЦ ОДЕЖДА',
+  'products-СУХИЕ СМЕСИ',
+  'products-ШЛИФОВАЛЬНЫЕ МАШИНКИ',
+  'products-ОБУВЬ',
+]
+
+export const getProducts = () => {
+  return products
+}
